e8435ba974010b78000bfc10cc12ee8092d0fc53
Abishek Anthony <abishek.anthony@zuehlke.com>
2023-11-21 11:18:23 +0100

    Part of Task 1: Fix external client (maybe add test)

--- Kotlin/Kotlin/src/main/kotlin/FakeExternalClient.kt
+++ Kotlin/Kotlin/src/main/kotlin/FakeExternalClient.kt
@@ -4,6 +4,6 @@ class FakeExternalClient {
     private val faker = Faker()
 
     fun getMessage(messageId: Int): String {
-        return "MessageId: $messageId -> $faker.lorem().sentence()"
+        return "MessageId: $messageId -> ${faker.lorem().sentence()}"
     }
 }
